49047083f8bb4a3f437596bca1ebba89e166243a
matheus1714 <matheussilvamartins1714@gmail.com>
2023-09-09 15:02:11 -0300

    create a filter to projects

--- js/script.js
+++ js/script.js
@@ -2,3 +2,20 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 }
+function filterProjects() {
+  const searchInput = document.getElementById("search")
+  const projectsList = document.querySelector("#projects ul")
+  const projects = projectsList.querySelectorAll("li")
+
+  const filter = searchInput.value.toUpperCase()
+
+  for (let i = 0; i < projects.length; i++) {
+    const project = projects[i].querySelector("a p")
+    const projectName = project.textContent || project.innerText
+    if (projectName.toUpperCase().indexOf(filter) > -1) {
+      projects[i].style.display = ""
+    } else {
+      projects[i].style.display = "none"
+    }
+  }
+}
